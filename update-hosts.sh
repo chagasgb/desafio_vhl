0c9eed85df16b66668ecc25df82421013d1d8b6a
GC <biel.silva.chagas@gmail.com>
2024-09-05 12:56:18 +0000

    alteracoes

--- update-hosts.sh
+++ update-hosts.sh
@@ -15,18 +15,12 @@ hosts_path="/etc/hosts"
 update_hosts() {
     # Verificar se a entrada já existe
     if grep -q "$hostname" "$hosts_path"; then
-        echo "[$(date)] Entrada para $hostname já existe." >> /var/log/update_hosts.log
+        echo "Entrada para $hostname já existe."
     else
-        echo "$ip_address $hostname" >> "$hosts_path"
-        echo "[$(date)] Adicionado: $ip_address $hostname" >> /var/log/update_hosts.log
+        # Adicionar entrada ao arquivo /etc/hosts silenciosamente
+        echo "$ip_address $hostname"
+        echo "Adicionado: $ip_address $hostname"
     fi
 }
 
-# Criar o arquivo de log se não existir
-touch /var/log/update_hosts.log
-
-# Executar a função de atualização
 update_hosts
-
-# Saída do container após execução
-exit 0
